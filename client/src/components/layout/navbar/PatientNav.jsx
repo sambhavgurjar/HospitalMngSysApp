29e030a46c88b4e82a076e53bd20a9d70badb17b
sambhavgurjar <sambhavgurjar9@gmail.com>
2025-08-09 19:58:25 +0530

    change in nav

--- client/src/components/layout/navbar/PatientNav.jsx
+++ client/src/components/layout/navbar/PatientNav.jsx
@@ -1,10 +1,8 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
 
 const PatientNav = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const navigate = useNavigate();
 
   const handleLogout = () => {
     localStorage.clear();
